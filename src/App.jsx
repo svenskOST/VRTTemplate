45f27aabcd2e79ed60712339b397a0ad86d28b2e
svenskOST <svenskost6@gmail.com>
2023-12-11 17:59:03 +0100

    updated the template to reflect current workstyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,15 @@
+import './index.css'
+import { Route, Routes } from 'react-router'
+
 function App() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center bg-slate-700">
-      <MainCont />
-    </div>
-  )
-}
-
-function MainCont() {
-  return (
-    <div className="flex h-3/4 w-3/4 max-w-md select-none flex-col items-center justify-center rounded-3xl bg-slate-800 text-3xl text-white shadow-lg shadow-gray-900 lg:w-1/3 lg:min-w-fit lg:p-8">
-      <TextCont />
-    </div>
-  )
-}
-
-function TextCont() {
-  return (
-    <>
-      <h1 className="text-2xl lg:text-4xl">This is my template for</h1>
-      <br />
-      <Link text={"Vite"} url={"https://vitejs.dev/"} />
-      <Link text={"React"} url={"https://react.dev/"} />
-      <Link text={"Tailwind"} url={"https://tailwindcss.com/"} />
-      <br />
-      <p className="text-lg lg:text-2xl">(With auto Tailwind class sorter)</p>
-    </>
-  )
-}
-
-function Link({ text, url }) {
-  return (
-    <>
-      <a className="relative duration-300 hover:text-purple-500 animate-bounce" href={url}>
-        {text}
-      </a>
-    </>
-  )
+   return (
+      <>
+         <Routes>
+            <Route path='/' element={<Page1 />}></Route>
+            <Route path='/page2' element={<Page2 />}></Route>
+         </Routes>
+      </>
+   )
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-    fontFamily: {
-      "sans": ["Titillium Web"]
-    }
+     extend: {
+        screens: {
+           xs: '440px',
+        },
+     },
+     fontFamily: {
+        sans: ['Poppins'],
+     },
   },
   plugins: [],
 }
-
